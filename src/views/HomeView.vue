22a425733d67261ff61aa10b84a6533bfc80e8ba
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2023-02-23 18:28:54 -0300

    finished lesson 18

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+import { ref } from "vue";
+
+let message = ref("Hello, World!");
+
+setTimeout(() => {
+  message.value = "Changed message.";
+}, 2000);
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
   </main>
 </template>
