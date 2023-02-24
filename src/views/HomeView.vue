4c51f04761628ee1cc14275c3250a41f07733454
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2023-02-23 22:40:36 -0300

    finished lesson 20

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,18 @@
 <script setup>
-import { useFlash } from "@/composables/useFlash";
+import { useStorage } from "@/composables/useStorage";
 
-let { flash } = useFlash();
+let food = useStorage("food");
+let age = useStorage("age");
 </script>
 
 <template>
   <main>
-    <button
-      type="button"
-      @click="flash('Please stop clicking me, it hurts so bad!')"
-    >
-      Click Me
-    </button>
+    <p>
+      What is your favorite food? <input type="text" v-model="food" />
+    </p>
+
+    <p>
+      How old are you? <input type="text" v-model="age" />
+    </p>
   </main>
 </template>
